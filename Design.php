f0779ddf889e2af04773cebc2e3810386fd4d67e
Arild Matsson <arild@klavaro.se>
2017-04-28 16:59:57 +0200

    Extracted half of Design to new Printie class

--- Design.php
+++ Design.php
@@ -7,13 +7,6 @@ require_once( 'fpdi/fpdi.php' );
 
 abstract class Design {
 
-	/**
-	 * The modifiable PDF object, if initiated.
-	 *
-	 * @var \FPDI|null
-	 */
-	protected $pdf;
-
 	public abstract function getName();
 
 	/**
@@ -33,53 +26,12 @@ abstract class Design {
 		return implode("\n", $fields);
 	}
 
-	protected abstract function decoratePdf($data);
-
-	public function preview($data) {
-		$this->initPage();
-		$this->decoratePdf($data);
-		$this->watermark();
-		$this->pdf->Output(NULL, 'I');
-
-	}
-
-	public function generate($filename, $data) {
-		$this->initPage();
-		$this->decoratePdf($data);
-		$this->pdf->Output($filename, 'F');
-	}
-
-	protected function initPage() {
-		$this->pdf = new \FPDI();
-		$this->pdf->SetPrintHeader(false);
-		$this->pdf->SetPrintFooter(false);
-		$this->pdf->setFontSubsetting(false);
-		$this->pdf->AddPage('P');
-		// Import template
-		$tpl_file = $this->getPdfPath();
-		$num_pages = $this->pdf->setSourceFile($tpl_file);
-		$page = $this->pdf->importPage(1);
-		$this->pdf->useTemplate($page, 0, 0);
-	}
-
-	protected function getPdfPath() {
-		$reflector = new \ReflectionClass(static::class);
-		return str_replace('.php', '.pdf', $reflector->getFileName());
-	}
-
-	protected function watermark() {
-		$this->pdf->SetTextColor( 150, 150, 150 );
-		$this->pdf->SetFont( 'gotham-medium', '', 14 );
-		$coor = array(
-			array(30,  40), array(130,  40),
-			array(30, 100), array(130, 100),
-			array(30, 180), array(130, 180),
-			array(30, 260), array(130, 260)
-		);
-		foreach( $coor as $pos ) {
-			$this->pdf->SetXY( $pos[0], $pos[1] );
-			$this->pdf->Write( 0, __('PREVIEW', 'butique') );
-		}
-	}
+	/**
+	 * @param \FPDI $pdf
+	 * @param array $data
+	 *
+	 * @return mixed
+	 */
+	public abstract function decoratePdf($pdf, $data);
 
 }
---
+++ Printie.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Printies;
+
+class Printie {
+
+	/**
+	 * @var \Printies\Design
+	 */
+	protected $design;
+
+	/**
+	 * @var array
+	 */
+	protected $data;
+
+	/**
+	 * The modifiable PDF object, if initiated.
+	 *
+	 * @var \FPDI|null
+	 */
+	protected $pdf;
+
+	public function __construct(Design $design, array $data) {
+		$this->design = $design;
+		$this->data = $data;
+	}
+
+	public function preview($watermark_text = 'PREVIEW') {
+		$this->initPage();
+		$this->design->decoratePdf($this->pdf, $this->data);
+		$this->watermark($watermark_text);
+		$this->pdf->Output(NULL, 'I');
+
+	}
+
+	public function generate($filename) {
+		$this->initPage();
+		$this->design->decoratePdf($this->pdf, $this->data);
+		$this->pdf->Output($filename, 'F');
+	}
+
+	protected function initPage() {
+		$this->pdf = new \FPDI();
+		$this->pdf->SetPrintHeader(false);
+		$this->pdf->SetPrintFooter(false);
+		$this->pdf->setFontSubsetting(false);
+		$this->pdf->AddPage('P');
+		// Import template
+		$tpl_file = $this->getPdfPath();
+		$num_pages = $this->pdf->setSourceFile($tpl_file);
+		$page = $this->pdf->importPage(1);
+		$this->pdf->useTemplate($page, 0, 0);
+	}
+
+	protected function getPdfPath() {
+		$reflection_object = new \ReflectionObject($this->design);
+		$class_filename = $reflection_object->getFileName();
+		return str_replace('.php', '.pdf', $class_filename);
+	}
+
+	protected function watermark($watermark_text) {
+		$this->pdf->SetTextColor( 150, 150, 150 );
+		$this->pdf->SetFont( 'gotham-medium', '', 14 );
+		$coor = array(
+			array(30,  40), array(130,  40),
+			array(30, 100), array(130, 100),
+			array(30, 180), array(130, 180),
+			array(30, 260), array(130, 260)
+		);
+		foreach( $coor as $pos ) {
+			$this->pdf->SetXY( $pos[0], $pos[1] );
+			$this->pdf->Write( 0, $watermark_text );
+		}
+	}
+
+}
